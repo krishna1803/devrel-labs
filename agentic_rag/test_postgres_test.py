a8c769e7c19944368168d0848de7c8fb7bf8c933
krishna1803 <krishna_kumar_sh@yahoo.com>
2025-08-04 10:06:12 +1000

    Changed the embedding model

--- agentic_rag/test_postgres_test.py
+++ agentic_rag/test_postgres_test.py
@@ -16,7 +16,7 @@ import pandas as pd
 
 
 
-model_name = "sentence-transformers/all-MiniLM-L12-v2"
+model_name = "sentence-transformers/all-MiniLM-L6-v2"
 embeddings = HuggingFaceEmbeddings(model_name=model_name)
 
 # See docker command above to launch a postgres instance with pgvector enabled.
