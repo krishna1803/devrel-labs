9fc2d387eaf0501544daeb2866ce15c1ca449da9
krishna1803 <krishna_kumar_sh@yahoo.com>
2025-08-05 10:56:25 +1000

    Added the New Sql for postgres

--- agentic_rag/test_postgres_test.py
+++ agentic_rag/test_postgres_test.py
@@ -20,7 +20,7 @@ model_name = "sentence-transformers/all-MiniLM-L6-v2"
 embeddings = HuggingFaceEmbeddings(model_name=model_name)
 
 # See docker command above to launch a postgres instance with pgvector enabled.
-connection = "postgresql+psycopg://postgres:RAbbithole1234##@192.168.105.32:5432/langchain"  # Uses psycopg3!
+connection = "postgresql+psycopg://postgres:RAbbithole1234##@192.168.105.32:5432/postgres"  # Uses psycopg3!
 #collection_name = "langchain_pg_collection"
 collection_name = "PDF Collection"
 
